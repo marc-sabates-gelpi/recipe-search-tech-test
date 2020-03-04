b7eecf8c6ba55c3fb06d008903bd5adba11a794a
Marc Sabates Gelpi <marc.sabates.gelpi@protonmail.com>
2020-03-04 19:15:29 +0000

    Add New index and register

--- src/rf_search/core.clj
+++ src/rf_search/core.clj
@@ -1,46 +1,60 @@
 (ns rf-search.core
-  (:require [clojure.java.io :refer [file as-file]]
+  (:require [clj-memory-meter.core :as mm]
+            [clojure.java.io :refer [file as-file]]
             [clojure.string :as string]
             [digest :as digest]
-            [clj-memory-meter.core :as mm]))
+            [taoensso.timbre :refer [debug spy]]))
 
-(def ^:private file-register (atom {}))
-(def ^:private index (atom {}))
+(defonce ^:private register (atom {}))
+(defonce ^:private index (atom {}))
 
 (defn file->index
-  [{:keys [id content]}]
+  "Return a map consisting of words as keys and
+  their frequency within a file."
+  [{:keys [id content] :as _file-details}]
   (->> content
        (re-seq #"[a-zA-Z]{4,}")
        (map string/lower-case)
        frequencies
-       (map (fn [[k v]] {k (list {:freq v :id id})}))
+       (map (juxt key (comp #(list {:freq % :id id}) val)))
        (into {})))
 
-(defn make-index
+(defn file->md5
+  "Return the md5sum of a given file `id` when it differs
+  from the registered one."
+  [id]
+  (let [md5 (digest/md5 (as-file id))]
+    (when-not (= (get @register id) md5)
+      {:id id :md5 md5})))
+
+(defn dir->files
+  "Return the files in a directory and their md5sum.
+  It runs the list against `register` and filters out
+  unchanged files."
   [s]
-  (let [files (->> s
-                   file
-                   file-seq
-                   (map str)
-                   rest                                     ;; first element is the dir
-                   )
-        ]
-    (reset! index (->> files
-                       (pmap (comp file->index #(array-map :id % :content (slurp %))))
-                       (reduce (partial merge-with into) {})
-                       (map (fn [[k v]] [k (reverse (sort-by :freq v))]))
-                       (take 10)
-                       (into {})))))
+  (->> s
+       file
+       file-seq
+       (map str)
+       rest                                                 ;; first element is the dir
+       (take 10)
+       (keep file->md5)))
+
+(defn get-index-register
+  [s]
+  (let [files    (dir->files s)
+        index    (->> files
+                      (pmap (comp file->index #(assoc % :content (slurp (:id %)))))
+                      (reduce (partial merge-with into) {})
+                      (map (juxt key (comp reverse (partial sort-by :freq) val)))
+                      (into {}))
+        register (into {} (map (juxt :id :md5) files))]
+    {:index index :register register}))
 
 (defn -main
   []
   (println "Starting indexing")
-  (make-index "resources/recipes"))
+  (get-index-register "resources/recipes"))
 
-(comment (time (->> "resources/recipes"
-                    file
-                    file-seq
-                    (map str)
-                    rest                                                  ;; first element is the dir
-                    (map #(vector % (digest/md5 (as-file %))))
-                    (into {}))))
+
+(comment (mm/measure (-main)))
